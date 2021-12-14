bcf8259e4cb1844056a160452038e1fd9a3990cb
Warren Haskins <warrenhaskins1@gmail.com>
2021-12-14 16:36:55 -0500

    added array of objects/questions

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -2,50 +2,35 @@
 var questions = [
   {
     qText: "Commonly used Data Types DO NOT include?:",
-    ans1: "strings",
-    ans2: "booleans",
-    ans3: "alerts",
-    ans4: "numbers",
-    ansCorrect: "3"
+    answer: ['strings', 'boolean', 'alerts', 'numbers'],
+    ansCorrect: 'alert'
   },
 
   {
     qText: "The condition in an if/else statement is enclosed with?:",
-    ans1: "quotes",
-    ans2: "parenthesis",
-    ans3: "curly braces",
-    ans4: "brackets",
-    ansCorrect: "2"
+    answer:['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
+    ansCorrect: "parenthesis"
   },
 
-  
+
   {
     qText: "Arrays in Javascript can be used to store?:",
-    ans1: "numbers & strings",
-    ans2: "booleans",
-    ans3: "other arrays",
-    ans4: "all of the above",
-    ansCorrect: "4"
+    answer:['numbers & strings', 'arrays', 'booleans', 'AotA'],
+    ansCorrect: "AotA"
   },
 
-  
+
   {
     qText: "String values must be enclosed within _ when being assigned to variables?:",
-    ans1: "commas",
-    ans2: "curly braces",
-    ans3: "quotes",
-    ans4: "parenthesis",
-    ansCorrect: "3"
+    answer:['commas', 'curly braces', 'quotes', 'parenthesis'],
+    ansCorrect: "quotes"
   },
 
-  
+
   {
     qText: "A very useful tool during development and debugging for printing content to the screen is?:",
-    ans1: "Javascript",
-    ans2: "Terminal/Bash",
-    ans3: "for loops",
-    ans4: "console.log()",
-    ansCorrect: "4"
+    answer:['JS', 'Terminal/Bash', 'for loops', 'console.log()'],
+    ansCorrect: "console.log()"
   },
 
 ];
